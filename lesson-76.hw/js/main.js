d7c127601b11345a3fe2d49b872b29548bd3229a
Dmitriy1973brod <153993588+Dmitriy1973brod@users.noreply.github.com>
2024-01-26 22:01:40 +0300

    add lesson-76.hw

---
+++ lesson-76.hw/js/main.js
@@ -0,0 +1,7 @@
+const firstName = 'Дмитрий'
+const lastName = 'Грицкевич'
+const gender = 'Мужчина'
+let mayStatus = 'Студент'
+let age = 50
+
+console.log(firstName, lastName, gender, mayStatus,  age, 'лет')
