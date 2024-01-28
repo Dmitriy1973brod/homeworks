5d831784b204ccf9384b5df29c21b408c3183d67
Dmitriy1973brod <153993588+Dmitriy1973brod@users.noreply.github.com>
2024-01-28 17:52:37 +0300

    add lesson-79.hw

---
+++ lesson-79.hw/js/main.js
@@ -0,0 +1,9 @@
+const age = 50
+
+if (age >= 18) {
+    console.log('Вы совершеннолетний') 
+} else {
+    console.log('Вам ещё нет 18') 
+}
+
+age >= 18 ? console.log('Вы совершеннолетний') : console.log('Вам ещё нет 18')
