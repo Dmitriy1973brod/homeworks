87dbc2a2ed57b18cd5cdf9989b3d330adfd5c8ce
Dmitriy1973brod <153993588+Dmitriy1973brod@users.noreply.github.com>
2024-01-28 17:53:19 +0300

    add lesson-80.hw

---
+++ lesson-80 .hw/js/main.js
@@ -0,0 +1,21 @@
+for (let i = 2; i <= 12; i = i + 2) {
+    console.log(i)
+}
+
+for (let i = 10; i > 0; i--) {
+    console.log(i)
+}
+
+let i = 0
+
+while (i < 12) {
+    i = i + 2
+    console.log(i)
+}
+
+let a = 11
+
+while (a > 0) {
+    a--
+    console.log(a)
+}
