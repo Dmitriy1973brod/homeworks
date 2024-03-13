ce9595b380545991526cf2fbd332c85a27fa97d2
Dmitriy1973brod <153993588+Dmitriy1973brod@users.noreply.github.com>
2024-03-13 22:32:38 +0300

    add lesson-113,114.hw

---
+++ lesson-113,114.hw/wake&bake/wake-bake/js/main.js
@@ -0,0 +1,82 @@
+(function() {
+
+// Бургер
+
+    document.addEventListener('click', burgerInit)
+
+    function burgerInit(e){
+        const burgerIcon = e.target.closest('.burger__menu')  
+        const burgerNavLink = e.target.closest('.nav__link')
+        
+        if (document.documentElement.clientWidth > 900) return
+
+        if(burgerIcon || burgerNavLink) {
+            document.body.classList.toggle('body--opened-menu')
+        } 
+    }
+
+    // Модалка
+
+    document.addEventListener('click', openModal)
+
+    function openModal(e){
+        const modalIcon = e.target.closest('.about__img-button')  
+        const closeModal = e.target.closest('.modal__cancel')       
+              
+        e.preventDefault() 
+
+        if(modalIcon || closeModal) {
+            document.body.classList.toggle('body--opened-modal')
+        } 
+    }
+})()
+
+// Табы
+
+const tabControls = document.querySelector('.tab__controls')
+
+tabControls.addEventListener('click', toggleTab)
+
+function toggleTab(e) {
+
+    const tabControl = e.target.closest('.tab__controls-link')
+
+    if (!tabControl) return
+    e.preventDefault
+    if (tabControl.classList.contains('tab__controls-link--activ')) return
+
+    const tabContentID = tabControl.getAttribute('href')
+    const tabContent = document.querySelector(tabContentID)
+    const acviveControl = document.querySelector('.tab__controls-link--activ')
+    const activeContent = document.querySelector('.tab-content--show')
+
+    acviveControl.classList.remove('tab__controls-link--activ')
+    activeContent.classList.remove('tab-content--show')
+
+    tabControl.classList.add('tab__controls-link--activ')
+    tabContent.classList.add('tab-content--show')
+
+}
+
+// Аккордеон
+ 
+const accordionLists = document.querySelectorAll('.accordion-list');
+
+accordionLists.forEach(el => {
+
+    el.addEventListener('click', (e) => {
+
+        const accordionControl = e.target.closest('.accordion-list__heading');
+        if (!accordionControl) return
+        const accordionItem = accordionControl.parentElement;
+        const accordionContent = accordionControl.nextElementSibling;
+
+        accordionItem.classList.toggle('accordion-list__item--opened');
+
+        if (accordionItem.classList.contains('accordion-list__item--opened')) {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+        } else {
+            accordionContent.style.maxHeight = null;
+        }
+    });
+});
